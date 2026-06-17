955700323a6a159b505919796163850021f75bef
ANA NUBIA DA SILVA <ana_nubia-silva@estudante.sesisenai.org.br>
2026-06-17 17:29:33 -0300

    Atividade 6

--- exercicio01.js
+++ exercicio01.js
@@ -45,3 +45,55 @@ if(prioridade == 1){
 }else{
     console.log("prioridade invalida");
 }
+
+//Exercício 06 — Central de Atendimento
+
+let opcao = 3
+let setor
+
+switch (opcao){
+    case 1: setor = "Financeiro"; break;
+    case 2: setor = "Suporte técnico"; break;
+    case 3: setor = "Multimídia"; break;
+    default: setor = "Opção inválida"; break;
+}
+console.log(setor);
+
+//Exercício 07 — Monitoramento de Processos
+
+for (let i = 0; i <= 10; i++){
+    console.log("Executando tarefa " + i);
+}
+
+//Exercício 08 — Relatório de Equipe
+
+let equipe = ['Julia', 'Ana', 'Henrique', 'Gabriel', 'Andressa'];
+for (let  i= 0; i < equipe.length; i++){
+console.log("Integrante: " + equipe[i]);
+}
+
+//Exercício 09 — Dashboard de Vendas
+
+let vendas = [100, 250, 400, 150, 300, 500, 200];
+
+let total = 0;
+let menor = vendas[0];
+let maior = vendas[0];
+
+for(let vendas of vendas){
+    total += vendas;
+
+    if(venda > maior){
+        maior = venda;
+    }
+    if (venda < menor){
+        menor = vemda;
+    }
+}
+
+let media = total/ vendas.length;
+
+console.log("Total:", total);
+console.log("Média:", media.toFixed(2));
+console.log("Maior venda:", maior);
+console.log("Menor venda:", menor);
