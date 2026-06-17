018c3b4f42dad18fa818d4bc6d433937b4a61302
ANA NUBIA DA SILVA <ana_nubia-silva@estudante.sesisenai.org.br>
2026-06-17 15:07:47 -0300

    Atividade 2

--- exercicio01.js
+++ exercicio01.js
@@ -9,4 +9,15 @@ let cargo = "Pedreiro";
 let tempoExperiencia = "15 anos";
 
 console.log("Nome proficional: " + nome);
-console.log("Cargo: " +)
+console.log("Cargo: " + cargo);
+console.log("Tempo de experiência: " + tempoExperiencia);
+
+//Exercício 03 — Cálculo de Bonificação
+
+let totalVendido = 5000;
+let bonificacao = totalVendido * 0.10;
+let valorTotal = totalVendido + bonificacao;
+
+console.log("Vendas: R$ " + totalVendido);
+console.log("Bonificação: R$ " + bonificacao);
+console.log("Total: R$ " + valorTotal);
