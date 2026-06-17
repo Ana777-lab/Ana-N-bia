9e83c065da4dbc8ef651a81e75af3fb4f34ddf57
ANA NUBIA DA SILVA <ana_nubia-silva@estudante.sesisenai.org.br>
2026-06-17 14:53:39 -0300

    Atividade 1

---
+++ exercicio01.js
@@ -0,0 +1,12 @@
+//Exercício 01 — Sistema de Boas-Vindas
+
+console.log("Bem-vindo ao ambiente de desenvolvimento JavaScript!")
+
+//Exercício 02 — Cadastro de Desenvolvedor
+
+const nome = "Adilsom";
+let cargo = "Pedreiro";
+let tempoExperiencia = "15 anos";
+
+console.log("Nome proficional: " + nome);
+console.log("Cargo: " +)
